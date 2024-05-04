8b9f513e43901a4ae952283831dfc912b9a2671f
TakeADev <chanceconwaydev@gmail.com>
2024-05-04 16:28:42 -0400

    Change client to pool. Start seed setup

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,29 +1,64 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express from 'express';
+import express, { query } from 'express';
 import ViteExpress from 'vite-express';
 const app = express();
 
 import pg from 'pg';
 
-const { Client } = pg;
-const client = new Client({
+const { Pool } = pg;
+import { User } from '../../lib/definitions.js';
+
+const pool = new Pool({
   user: process.env.POSTGRESDB_USER,
   password: process.env.POSTGRESDB_PASSWORD,
   host: process.env.POSTGRESDB_HOST,
   database: process.env.POSTGRESDB_DATABASE,
 });
 
-await client.connect();
+async function seedUsers(users: User[]) {
+  // await pool.query(`
+  //   CREATE TABLE users (
+  //   display_name varchar(20),
+  //   name varchar(20),
+  //   bio varchar(250),
+  //   collections text[]
+  // );`);
+  //
+  const queryText = `INSERT INTO users VALUES($1, $2, $3, $4);`;
+  try {
+    users.map((user) => {
+      pool.query(queryText, [user.displayName, user.name, user.bio, user.collections]);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-const cres = await client.query('SELECT $1::text as message', ['Hello world!']);
+const usersToSeed: User[] = [
+  {
+    displayName: 'testy',
+    name: 'test boy',
+    bio: 'this is a bio with spaces',
+    collections: ['cards1', 'cards2', 'cards3'],
+  },
+];
 
 const PORT = process.env.SERVER_PORT || 8080;
 
-app.get('/hello', (_, res) => {
-  console.log(cres.rows[0].message);
+app.get('/seed', (_, res) => {
+  seedUsers(usersToSeed);
   res.send('Hello Vite + React + TypeScript!');
+});
+
+app.get('/getusers', (_, res) => {
+  pool.query('SELECT * FROM users;').then((resp) => res.send(resp.rows));
+});
+
+app.get('/deleteusers', (_, res) => {
+  pool.query('DELETE FROM users;');
+  res.send('users deleted');
 });
 
 ViteExpress.listen(app, PORT as number, () =>
