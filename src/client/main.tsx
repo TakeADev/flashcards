31f1b746368b86c5f8079f34671ff8cef4e634e6
TakeADev <chanceconwaydev@gmail.com>
2024-05-05 17:11:42 -0400

    Setup router

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,12 +1,23 @@
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from "./App";
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import Root from './pages/root/Root';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
