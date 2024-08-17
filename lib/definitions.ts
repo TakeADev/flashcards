6eb633bcc1acbe43529a5abe6ed377a745219d6a
TakeADev <chanceconwaydev@gmail.com>
2024-08-17 13:30:02 -0400

    Differentiate client and DB user interfaces.

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,6 +1,16 @@
-export interface User {
-  id: string;
-  email: string;
-  displayName: string;
-  collections: string[];
+export namespace User {
+  export interface DBUser {
+    id: string;
+    email: string;
+    displayName: string;
+    collections: string[];
+    password: string;
+  }
+
+  export interface ClientUser {
+    id: string;
+    email: string;
+    displayName: string;
+    collections: string[];
+  }
 }
