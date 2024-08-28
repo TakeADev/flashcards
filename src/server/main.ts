abc18c619821c85f3874974b51e82934858c044e
TakeADev <chanceconwaydev@gmail.com>
2024-08-27 23:50:33 -0400

    Implement various changes regarding authentication - import jwt and uuid packages - remove unnecessary verifyUser middleware - change route names; adding /api/auth to post routes handling   authentication - user is assigned id on signup - create login functionality - create route for verifying jwt

--- src/server/main.ts
+++ src/server/main.ts
@@ -5,6 +5,10 @@ import express from 'express';
 import ViteExpress from 'vite-express';
 import bodyParser from 'body-parser';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { v4 as uuidv4 } from 'uuid';
+
+import { User } from '../../lib/definitions.js';
 
 import * as db from './db/index.js';
 
@@ -12,26 +16,14 @@ const PORT = process.env.SERVER_PORT || 8080;
 const app = express();
 app.use(bodyParser.json());
 
-function verifyUser(email: string, password: string) {
-  try {
-    db.query(`SELECT * FROM users WHERE email=($1);`, [email]).then((queryResult) =>
-      bcrypt.compare(password, queryResult.rows[0].password).then((comparison) => {
-        return comparison;
-      })
-    );
-  } catch (error) {
-    return error;
-  }
-}
-
-app.post('/signup', async (req, res, next) => {
-  const { email, name, password } = req.body;
+app.post('/api/auth/signup', async (req, res, next) => {
+  const { email, displayName, password } = req.body;
   bcrypt.hash(password, 10, (err, hash) => {
     if (err) return alert(err);
     try {
       return db.query(
-        `INSERT INTO users(email, name, password, collections) VALUES($1, $2, $3, $4)`,
-        [email, name, hash, []]
+        `INSERT INTO users(id, email, display_name, password, collections) VALUES($1, $2, $3, $4, $5)`,
+        [uuidv4(), email, displayName, hash, []]
       );
     } catch (error) {
       console.log(error);
@@ -39,12 +31,38 @@ app.post('/signup', async (req, res, next) => {
   });
 });
 
-app.get('/fail', (req, res) => {
-  res.send('failed');
+app.post('/api/auth/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const foundUser: User.DBUser = await db
+      .query(`SELECT * FROM users WHERE email=($1);`, [email])
+      .then((res) => res.rows[0]);
+
+    if (!foundUser) return res.status(401).json({ error: 'Invalid email or password.' });
+
+    const passwordMatch = await bcrypt.compare(password, foundUser.password);
+    if (!passwordMatch) return res.status(401).json({ error: 'Invalid email or password.' });
+
+    const token = jwt.sign({ userId: foundUser.id }, process.env.JWT_SECRET || 'secret', {
+      expiresIn: '1h',
+    });
+
+    res.status(200).json({ token: token, email: email });
+  } catch (error) {
+    res.status(500).json({ error: 'Authentication error' });
+  }
 });
 
-app.post('/login', (req, res) => {
-  return;
+app.post('/api/auth/verify', async (req, res) => {
+  let token = req.body.token;
+
+  jwt.verify(token, process.env.JWT_SECRET || 'secret', (err, decoded) => {
+    if (err?.name === 'TokenExpiredError') {
+      res.status(200).json({ token: false });
+    } else if (err) {
+      res.status(401).json({ error: err });
+    } else res.status(200).json({ token: decoded });
+  });
 });
 
 ViteExpress.listen(app, PORT as number, () =>
